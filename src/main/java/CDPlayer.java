cce9ed382bfdec2079350e2d3b69454280fdae93
Douglas MacIntyre <douglasmacintyre@Douglass-iMac.fritz.box>
2020-11-24 15:02:05 +0000

    Built the CD Changer

--- src/main/java/CDPlayer.java
+++ src/main/java/CDPlayer.java
@@ -1,2 +1,21 @@
-public class CDPlayer {
+public class CDPlayer extends Component {
+
+    private int CDCapacity;
+    private int cdsInPlayer;
+
+    public CDPlayer(String make, String model, int CDCapacity) {
+        super(make, model);
+        this.CDCapacity = CDCapacity;
+        cdsInPlayer = 0;
+    }
+
+    public void addCD(int numberOfCDsToAdd) {
+        if (this.cdsInPlayer < this.CDCapacity) {
+            this.cdsInPlayer += numberOfCDsToAdd;
+        }
+    }
+
+    public int getNumberOfCDs(){
+        return this.cdsInPlayer;
+    }
 }
--- src/test/java/CDPlayerTest.java
+++ src/test/java/CDPlayerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CDPlayerTest {
+
+    private CDPlayer shinyCDPlayer;
+
+    @Before
+    public void before() {
+        shinyCDPlayer = new CDPlayer("Shiny", "Silver", 5);
+    }
+
+    @Test
+    public void cdPlayerCanHoldCDs(){
+        shinyCDPlayer.addCD(1);
+        assertEquals(1, shinyCDPlayer.getNumberOfCDs());
+    }
+
 }
